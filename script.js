ab861e426a50fe1ec9ee33a7be57e44f9cc7e0f5
RenataFerreira96 <130588601+RenataFerreira96@users.noreply.github.com>
2023-08-03 21:47:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstNumber = prompt("Digite o primeiro número")
+let secondNumber = prompt("Digite o segundo número")
+
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+
+const sum = firstNumber + secondNumber
+const sub = firstNumber - secondNumber
+const mult = firstNumber * secondNumber
+const div = firstNumber / secondNumber
+const restdiv = firstNumber % secondNumber
+
+
+alert ('A soma dos dois números é: ' + sum)
+alert ('A subtração dos dois números é: ' + sub)
+alert ('A multiplicação dos dois números é: ' + mult)
+alert ('A divisão dos dois números é: ' + div)
+alert ('O resto da divisão dos dois números é: ' + restdiv)
+
+if(sum % 2 === 0 )
+alert ('A soma dos dois números é par!')
+else(sum % 2 !== 0)
+alert ('A soma dos dois números é impar!')
